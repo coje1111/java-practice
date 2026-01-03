b6983817977d957401a882d2176fa7b2a2a927b6
전명훈 <acekid21@naver.com>
2026-01-03 23:33:04 +0900

    2026-01-03: calculate discount and VAT

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int burgerPrice = 5200;
-        int quantity = 2;
+        int price = 12900;
+        int quantity = 3;
 
-        double discountRate = 0.15;
+        double discountRate = 0.12;
+        double vatRate = 0.10;
 
-        int totalPrice = burgerPrice * quantity;
+        int totalPrice = price * quantity;
         int discountAmount = (int) (totalPrice * discountRate);
-        int finalPrice = totalPrice - discountAmount;
-
+        int discountedPrice = totalPrice - discountAmount;
+        int vatAmount = (int) (discountedPrice * vatRate);
+        int finalPrice = discountedPrice + vatAmount;
 
         System.out.println(finalPrice);
     }
