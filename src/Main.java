1fee38e3b41f45d08b3b1c5c942bd67c67796994
전명훈 <acekid2120@gmail.com>
2026-01-05 23:09:41 +0900

    2026-01-05: practice conditional discount logic

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        int price = 12900;
+        int price = 12000;
         int quantity = 3;
-        String grade = "VIP";
+        boolean isVip = false;
 
         int totalPrice = price * quantity;
         double discountRate;
 
-// 조건문 작성
-        if (grade.equals("VIP")) {
+        if (isVip) {
             discountRate = 0.2;
         } else if (totalPrice >= 30000) {
             discountRate = 0.1;
+        } else if (totalPrice >= 10000) {
+            discountRate = 0.05;
         } else {
-            discountRate = 0.0;
+            discountRate = 0;
         }
 
-        int discountAmount = (int)(totalPrice * discountRate);
+        int discountAmount = (int) (totalPrice * discountRate);
         int finalPrice = totalPrice - discountAmount;
 
-        System.out.println(grade);
-        System.out.println(discountRate);
-        System.out.println(discountAmount);
         System.out.println(finalPrice);
-        //26-01-04 조건문이 어떻게 굴러가는가 이해중
     }
 }
 
