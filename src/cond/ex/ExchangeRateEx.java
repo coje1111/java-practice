457202f133a2f35db082f5967984fc363bdd456f
전명훈 <acekid2120@gmail.com>
2026-05-20 01:37:52 +0900

    환율계산 if문 문제풀이

--- src/cond/ex/ExchangeRateEx.java
+++ src/cond/ex/ExchangeRateEx.java
@@ -15,7 +15,7 @@ public class ExchangeRateEx {
       System.out.println("환전할 금액이 없습니다.");
     } else {
 
-      int won = (dollar * 1300);
+      int won = dollar * 1300;
 
       System.out.println("환전 금액은 " + won + "원입니다.");
     }
