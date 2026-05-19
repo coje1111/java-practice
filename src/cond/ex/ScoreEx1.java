0872664ef8a7dff236eabdca64a5429b5fbee4aa
전명훈 <acekid2120@gmail.com>
2026-05-19 18:57:31 +0900

    if문 문제풀이 하드코딩

---
+++ src/cond/ex/ScoreEx1.java
@@ -0,0 +1,30 @@
+package cond.ex;
+
+public class ScoreEx1 {
+
+  public static void main(String[] args) {
+
+    int score = 71;
+
+    if (score >= 90 && score <= 100) {
+      System.out.println("score: " + score);
+      System.out.println("출력: 학점은 A입니다.");
+    } else if (score >= 80 && score <= 90) {
+      System.out.println("score: " + score);
+      System.out.println("출력: 학점은 B입니다.");
+
+    } else if (score >= 70 && score <= 80) {
+      System.out.println("score: " + score);
+      System.out.println("출력: 학점은 C입니다.");
+
+    } else if (score >= 60 && score <= 70) {
+      System.out.println("score: " + score);
+      System.out.println("출력: 학점은 D입니다.");
+
+    } else if (score < 60) {
+      System.out.println("score: " + score);
+      System.out.println("출력: 학점은 F입니다.");
+
+    }
+  }
+}
