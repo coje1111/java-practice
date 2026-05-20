306fdeb9fe542d69c0a180035c40e0592cbfdf6f
전명훈 <acekid2120@gmail.com>
2026-05-20 10:05:38 +0900

    while문 변수 i 사용

--- src/loop/While2_2.java
+++ src/loop/While2_2.java
@@ -4,7 +4,7 @@ public class While2_2 {
 
   public static void main(String[] args) {
     int sum = 0;
-    int i = 1;
+    int i = 10;
 
     sum = sum + i; //sum(0) + 1 -> sum(1)
     System.out.println("i=" + i + " sum=" + sum);
